e3c304820f08f47e4567985cdab7746f15ea81cd
Shuvo Baroi <contact.shuvobaroi@gmail.com>
2026-01-15 00:20:01 +0600

    Upgrade: added and used SMTP instead of sendgrid.

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,36 @@
+// module.exports = ({ env }) => ({
+//     email: {
+//       config: {
+//         provider: 'sendgrid',
+//         providerOptions: {
+//           apiKey: env('SENDGRID_API_KEY'),
+//         },
+//         settings: {
+//           defaultFrom: 'booking@bergamo-transfers.com',
+//           defaultReplyTo: 'booking@bergamo-transfers.com',
+//         },
+//       },
+//     },
+//   });
+
+
 module.exports = ({ env }) => ({
-    email: {
-      config: {
-        provider: 'sendgrid',
-        providerOptions: {
-          apiKey: env('SENDGRID_API_KEY'),
-        },
-        settings: {
-          defaultFrom: 'booking@bergamo-transfers.com',
-          defaultReplyTo: 'booking@bergamo-transfers.com',
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env.int('SMTP_PORT', 587),
+        secure: env('SMTP_SECURE', false), // Use `true` for port 465
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
         },
       },
+      settings: {
+          defaultFrom: 'booking@milan-transfers.com',
+          defaultReplyTo: 'booking@milan-transfers.com',
+        },
     },
-  });
+  },
+});
